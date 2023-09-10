1748cb11185e1f585f7092281a55e284a6dda33e
chrimbys <104105077+chrimbys@users.noreply.github.com>
2023-09-11 07:05:51 +0900

    newversion
    asset upload

--- Assets/Scripts/Main/SkyController.cs
+++ Assets/Scripts/Main/SkyController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System;
 
 public class SkyController : MonoBehaviour
 {
@@ -14,31 +15,73 @@ public class SkyController : MonoBehaviour
     SoundManager sm;
     [SerializeField]
     GetWeatherScript gw;
-    public bool bNight = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    public bool bNight = false;//鳥のSEの再生、停止に使用
+
+    int time;
     // 5~7:朝,8~14:昼,15~17:夕方,18~4:夜
     void Update()
     {
-        var lightPos = dLight.transform.eulerAngles;
-        if(lightPos.x > 360)
+        time = int.Parse(DateTime.Now.Hour.ToString());
+        if(time >= 18 && time < 24 && gw.bGetW)//曇りかどうかは関係なし
+        {
+            bNight = true;
+            RenderSettings.skybox = skyboxes[0];//夜
+        }
+        else if (time >= 0 && time < 5)
+        {
+            bNight = true;
+            RenderSettings.skybox = skyboxes[0];//夜
+        }
+        else if (!gw.bCloud && gw.bGetW)//晴れの時
+        {
+            if (time >= 5 && time < 8)
+            {
+                bNight = false;
+                RenderSettings.skybox = skyboxes[1];//朝
+            }
+            else if (time >= 8 && time < 15)
+            {
+                bNight = false;
+                RenderSettings.skybox = skyboxes[2];//昼
+            }
+            else if (time >= 15 && time < 18)
+            {
+                bNight = false;
+                RenderSettings.skybox = skyboxes[3];//夕方
+            }
+        }
+        else if (gw.bCloud && gw.bGetW)//曇りの時
+        {
+            if (time >= 5 && time < 18)
+            {
+                bNight = false;//夜以外は何もなしフラグ変更のみ
+            }
+        }
+
+        /*var lightPos = dLight.transform.eulerAngles;
+        if (lightPos.x > 360)
         {
             lightPos.x -= 360;
         }
         //Debug.Log(lightPos);
-        if (180 <= lightPos.x && lightPos.x < 345 && gw.bGetW)
+        if (180 <= lightPos.x && lightPos.x < 345 && gw.bGetW)//天気取得後、夜の場合
         {
-            bNight = true;
+            //bNight = true;//鳥のSEを止める
+            sm.smStop = true;
             RenderSettings.skybox = skyboxes[0];
             //Debug.Log("night");
         }
-        else if (!gw.bCloud && gw.bGetW)
+        else if (!gw.bCloud && gw.bGetW)//天気取得後、曇りでない場合(曇りの場合は夜以外曇りのskybox使用) 
         {
-            if ((345 <= lightPos.x && lightPos.x < 360) || (0 <= lightPos.x && lightPos.x < 30))
+            //bNight = false;
+            sm.smStop = false;
+            if (345 <= lightPos.x && lightPos.x < 360)
             {
-                bNight = false;
+                RenderSettings.skybox = skyboxes[1];
+                //Debug.Log("dawn");
+            }
+            else if (0 <= lightPos.x && lightPos.x < 30)
+            {
                 RenderSettings.skybox = skyboxes[1];
                 //Debug.Log("dawn");
             }
@@ -51,8 +94,7 @@ public class SkyController : MonoBehaviour
             {
                 RenderSettings.skybox = skyboxes[3];
                 //Debug.Log("evening");
-            }
-        }
+            }*/
     }
 }
 
--- Assets/Scripts/Main/SoundManager.cs
+++ Assets/Scripts/Main/SoundManager.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    //[SerializeField]
-    //AudioSource rain;
+    //雨のみオブジェクトセットアクティブ時再生されるため別管理とする
     [SerializeField]
     AudioSource bird;
     [SerializeField]
@@ -14,30 +13,38 @@ public class SoundManager : MonoBehaviour
     SkyController sc;
     [SerializeField]
     GetWeatherScript gw;
+    //public bool smStart, smStop;//鳥の声再生、停止
     // Start is called before the first frame update
     void Start()
     {
-        // bgm = GetComponent<AudioSource>();
         bgm.Play();
-        StartCoroutine(BirdSound());
     }
     void Update()
     {
-       
+        if(!gw.bRain && !sc.bNight)//夜でない、雨＆雪ではない.bNightはSkyController Update内にあるため読み取り専用
+        {
+            gw.bRain = true;
+            StartCoroutine(BirdSound());
+        }
     }
-    /*public void RainSound()
-    {
-        rain.Play();
-    }*/
     private IEnumerator BirdSound()
     {
-        while (!gw.bRain && !sc.bNight && gw.bGetW)
+        /*while (!smStop || !sc.bNight)//夜or雨or雪になるまで続く
         {
-            float i = Random.Range(4, 61);
+            float i = Random.Range(4, 10);
             Debug.Log("soundfloat i =" + i);
+            yield return new WaitForSeconds(i);
+            bird.Play();
+        }*/
+        while (!gw.bRain2 && !sc.bNight)//夜or雨or雪になるまで続く
+        {
+            float i = Random.Range(15, 61);
+            //Debug.Log("soundfloat i =" + i);
             yield return new WaitForSeconds(i);
             bird.Play();
         }
 
     }
+
+
 }
